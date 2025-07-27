e810ff84de0fd1d553c618af618d7ac3c1fb6591
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2025-07-27 11:24:55 -0600

    feat: cy.press use case added

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,6 +1,16 @@
 describe('My First Test', () => {
-  it('Does not do much!', () => {
-    cy.visit('http://localhost:3000')
-    cy.contains('Sign in')
-  })
+    beforeEach(() => {
+        cy.visit('http://localhost:3000')
+    })
+    it('cy.press testing', () => {
+        // https://docs.cypress.io/api/commands/press
+        // Unlike cy.type(), which is best for typing character keys,
+        // cy.press() will dispatch real keyboard events rather than simulated ones.
+        // This command is especially useful when testing focus management and keyboard navigation 
+        // _ patterns which are critical for accessibility testing and great keyboard UX.
+        cy.get('[data-testid="login-username"]').focus()
+        cy.press(Cypress.Keyboard.Keys.TAB)
+        cy.press(Cypress.Keyboard.Keys.TAB)
+        cy.get('[data-testid="login-password"]').should('have.focus')
+    })
 })
