425360c536eff581e650f86e816a7eed49a542b5
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2025-08-03 00:40:43 -0600

    feat: some fixes to standarize & clarify things

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -1,17 +1,18 @@
 import { test, expect } from '@/playwright/fixtures/index.fixtures';
+import data from '@/playwright/data/testData.json';
 
-const username = 'test_user';
-const password = 'test_pass';
-const incorrectPassword = 'incorrect_pass';
+
+// Username and password are sensitive data. I am using this approach just for demo purposes. 
+// In real life, I would use environment variables or a separate file for this.
 
 test.describe('Login & Logout Scenarios', () => {
-  test('login and logout workflow', async ({ e2e }) => {
-    await e2e.loginAndLogout(username, password);
+  test('should_login_and_logout_successfully', async ({ e2e }) => {
+    await e2e.loginAndLogout(data.username, data.password);
   });
 
-  test('login with incorrect password', async ({ loginPage }) => {
+  test('should_not_login_with_invalid_credentials', async ({ loginPage }) => {
     await loginPage.load();
-    await loginPage.submitSignInForm(username, incorrectPassword);
+    await loginPage.submitSignInForm(data.username, data.incorrectPassword);
     await expect(loginPage.errorContainer).toHaveText(/There was a problem.*Invalid username or password/);
   })
 });
